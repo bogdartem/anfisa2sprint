7cfc8eea4aff2c76d587f7b2e14b4e196732b479
Богданов Артём <bogdartem1997@yandex.ru>
2023-05-14 20:45:40 +0400

    Add new templates and redact views and models

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -8,11 +8,11 @@ def index(request):
     template = 'homepage/index.html'
 
     ice_cream_list = IceCream.objects.values(
-        'id', 'title', 'description'
+        'id', 'title', 'price', 'description'
     ).filter(
-        Q(is_published=True) & (
-            Q(is_on_main=True) | Q(title__contains='пломбир')
-        )
+        is_published=True,
+        is_on_main=True,
+        category__is_published=True
     )
     context = {
         'ice_cream_list': ice_cream_list,
